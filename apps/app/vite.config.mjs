0432b4185cb67cfc47c2c3390c5b2a41c859fbd6
Christopher Perzanowski <k.pezanovski@gmail.com>
2024-11-04 11:26:06 +0100

    Added vitest

---
+++ apps/app/vite.config.mjs
@@ -0,0 +1,14 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { configDefaults, defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    exclude: [...configDefaults.exclude],
+    include: ['**/?(*.){test,spec}.?(c|m)[jt]s?(x)'],
+    setupFiles: ["./src/config/setupTests.js"],
+  },
+});
