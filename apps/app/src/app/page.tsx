5ff67e8f956965c6ef8e83b0fbf9f0f070002d9b
Christopher Perzanowski <k.pezanovski@gmail.com>
2024-11-05 12:41:45 +0100

    Added changeset

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <Card title="web app" href="http://www.google.com">
-        App web 1
+        App web 1 heading test
       </Card>
     </main>
   );
